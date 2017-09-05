99afd4dbd3b62d13736124ef045e17f5a816fd22
omzeybek <omzeybek@gmail.com>
2017-09-05 06:51:17 +0300

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,6 @@
 #this code reads data into R, using provided attributes of data set 
 hpc <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=T, comment.char="", quote='\"')
+                nrows=2075259, check.names=F, stringsAsFactors=T, comment.char="", quote='\"')
 
 #Now i will subset two distinct dates which all the work will be done 
 
@@ -13,6 +13,7 @@ hpc_filt$Date<-as.Date(hpc_filt$Date,format="%d/%m/%Y")
 head(hpc_filt)
 
 #plot 1 
- hist(hpc_filt$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts",ylab="Frequency",col="Red")
- png("plot1.png", width=480, height=480)
- dev.off()
+png("plot1.png", width=480, height=480)
+hist(hpc_filt$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts",ylab="Frequency",col="Red")
+
+dev.off()
